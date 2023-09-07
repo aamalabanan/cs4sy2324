fab64089bea3c4678407aa63c6ecf8e43e4121cb
aamalabanan <112681440+aamalabanan@users.noreply.github.com>
2023-09-07 21:10:55 +0800

    CS4CharmMalabanan1stQEx1.java

--- src/cs4charmmalabanan/CS4CharmMalabanan.java
+++ src/cs4charmmalabanan/CS4CharmMalabanan.java
@@ -1,13 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package cs4charmmalabanan;
 
 /**
  *
- * @author CHARM
+ * @author amuel
  */
 public class CS4CharmMalabanan {
 
@@ -16,8 +15,58 @@ public class CS4CharmMalabanan {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String greeting = "Hello World.";
-        System.out.println(greeting + "Hello Pisay.");
+        String kidol = "Korean Idol";
+        String name = "Name: ";
+        String age = "age: ";
+        String height = "height: ";
+        
+        
+        // korean idol one
+        System.out.println(kidol + "1");
+        String hee = "Heeseung";
+        int heeage = 21; 
+        int heeheight = 183; 
+        System.out.println(name + hee);
+        System.out.println(age + heeage);
+        System.out.println(height + heeheight);
+        
+          System.out.println();
+          
+        // korean idol two
+        System.out.println(kidol + "2");
+        String nk = "Niki";
+        int nkage = 17;
+        int nkheight = 183; 
+        System.out.println(name + nk);
+        System.out.println(age + nkage);
+        System.out.println(height + nkheight);
+        
+          System.out.println();
+          
+        // korean idol three
+        System.out.println(kidol + "3");
+        String jk = "Jake";
+        int jkage = 20;
+        int jkheight = 175;
+        System.out.println(name + jk);
+        System.out.println(age + jkage);
+        System.out.println(height + jkheight);
+        
+          System.out.println();
+          
+          
+        nkage -= 0.1;
+        jkage += 1;
+        boolean nkageislower = nkage < jkage;
+        System.out.println("Is Niki younger than Jake?");
+        System.out.println("the statement is " + nkageislower + " since Niki is younger than Jake.");
+        
+        heeheight += 1;
+        jkheight -= 0.1;
+        boolean heeishigher = heeheight < jkheight;
+        System.out.println("Is Jake taller than Heeseung?");
+        System.out.println("the statement is " + heeishigher + " since Jake is shorter than Heeseung.");
+        
     }
     
 }
